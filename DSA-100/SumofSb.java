d8269aeb1d4dd23553ee75a3718f0c8cde0865b6
SanchitH17 <sanchithanda7@gmail.com>
2023-10-18 12:58:49 +0530

    DSA 100

---
+++ DSA-100/SumofSb.java
@@ -0,0 +1,21 @@
+public class SumofSb {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10};
+        int minsum = arr[0] ; 
+        int maxsum = arr[0] ;
+        
+        int sum ;
+        for(int i=0 ; i < arr.length ; i++) {
+             for(int j = i ; j < arr.length ; j++){
+                sum  = 0 ;
+                for(int k = i ; k <= j ; k++){
+                    sum+=arr[k];
+                }
+                maxsum = Math.max(maxsum,sum);
+                minsum = Math.min(sum,minsum);
+             }
+        }
+        System.out.println("The maximum sum is "+maxsum);
+        System.out.println("The minimum sum is "+minsum);
+    }
+}
